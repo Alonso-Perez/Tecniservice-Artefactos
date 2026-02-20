e0c30e28ef1d780cffaf17899caaa7a2e1bd493c
Alonso-Perez <143358797+Alonso-Perez@users.noreply.github.com>
2026-02-20 14:36:03 -0500

    initial commit for tecniservice-frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {CommonModule} from '@angular/common'; //Necesario para *ngFor y *ngIf
+//Ajusta la ruta de importación si es necesario, debe ser así:
+import { OrdenService } from './services/orden.service';
+import { Orden } from './models/orden.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('tecniservice-frontend');
+
+export class App implements OnInit {
+  listaOrdenes: Orden[] = [];
+  cargando = true;
+
+  constructor(private ordenService: OrdenService) { }
+
+  ngOnInit(): void {
+    this.cargarOrdenes();
+  }
+
+  cargarOrdenes(){
+    this.ordenService.getOrdenes().subscribe({
+      next: (datos) => {
+        console.log('Datos recibidos:', datos);
+        this.listaOrdenes = datos;
+        this.cargando = false;
+      },
+      error: (error) => {
+        console.error('Error conectado con Spring Boot:', error);
+        this.cargando = false;
+      }
+    });
+  }
+
 }
